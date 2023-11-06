0392578aab08682557dbd3e0ac5a354e5bec1033
PRAAAAA123 <prathameshdutondkar97@gmail.com>
2023-11-06 17:37:42 +0530

    Update the changes

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { Table } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Table, Modal, Button, InputNumber, Input, Spin, message } from 'antd';
+import axios from 'axios';
 
-
-const fallbackImageSrc = "https://www.ncenet.com/no-image-found"; 
+const fallbackImageSrc = 'your-fallback-image-url';
+const apiUrl = 'http://localhost:8080/add-user';
 
 const columns = [
   {
@@ -21,45 +22,112 @@ const columns = [
     title: 'Title',
     dataIndex: 'Title',
     key: 'Title',
-    render: (title) => title || 'No data available',
   },
   {
     title: 'Price',
     dataIndex: 'Variant Price',
     key: 'Variant Price',
-    render: (price) => price || 'No data available',
   },
   {
     title: 'SKU',
     dataIndex: 'Variant SKU',
     key: 'Variant SKU',
-    render: (sku) => sku || 'No data available',
   },
   {
     title: 'Vendor',
     dataIndex: 'Vendor',
     key: 'Vendor',
-    render: (vendor) => vendor || 'No data available',
+  },
+  {
+    title: 'Action',
+    dataIndex: '_id',
+    key: '_id',
+    render: (id) => (
+      <AddToCartButton itemId={id} />
+    ),
   },
 ];
 
+const AddToCartButton = ({ itemId }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const handleAddToCart = () => {
+    setLoading(true);
+    axios.post(apiUrl, { itemId, quantity })
+      .then((response) => {
+        setLoading(false);
+        message.success('Item added to the cart successfully.');
+      })
+      .catch((error) => {
+        setLoading(false);
+        message.error('Failed to add item to the cart.');
+      });
+
+    setModalVisible(false);
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={() => setModalVisible(true)}>
+        Add to Cart
+      </Button>
+      <Modal
+        title="Add to Cart"
+        visible={modalVisible}
+        onOk={handleAddToCart}
+        onCancel={() => setModalVisible(false)}
+      >
+        <p>Item ID: {itemId}</p>
+        <InputNumber
+          min={1}
+          value={quantity}
+          onChange={(value) => setQuantity(value)}
+        />
+        {loading && <Spin />}
+      </Modal>
+    </>
+  );
+};
+
 const Home = () => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
-    // Replace this URL with your API endpoint
-    fetch('http://localhost:8080/users')
-      .then((response) => response.json())
-      .then((result) => {
-        setData(result); // Assuming the API response is an array of items
+    axios.get('http://localhost:8080/users')
+      .then((response) => {
+        setLoading(false);
+        setData(response.data);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        setLoading(false);
+        message.error('Failed to fetch data.');
+      });
   }, []);
+
+  const filteredData = data.filter((item) =>
+    item['Variant SKU']?.includes(searchText) || item['Title']?.toLowerCase()?.includes(searchText?.toLowerCase())
+  );
 
   return (
     <div>
       <h1>Order Page</h1>
-      <Table dataSource={data} columns={columns} rowKey="_id" />
+      <Input
+        placeholder="Search by SKU or Title"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        style={{width:"60vw"}}
+      />
+      {loading ? (
+        <Spin size="large" />
+      ) : filteredData.length > 0 ? (
+        <Table dataSource={filteredData} columns={columns} rowKey="_id" />
+      ) : (
+        <p>No data available</p>
+      )}
     </div>
   );
 };
